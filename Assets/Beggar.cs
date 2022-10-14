8d0cf7cc78290b24a92d7dce0d628a9be5015df9
Mikinzi (frozencat3030) <112668967+mstrykul1336@users.noreply.github.com>
2022-10-13 19:03:06 -0700

    Add files via upload

---
+++ Assets/Beggar.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pattern.FactoryMethod
+{
+    public class Beggar : INPC
+    {
+        public void Speak()
+        {
+            Debug.Log("Beggar: Do you have some change to spare?");
+        }
+    }
+}
---
+++ Assets/NPCType.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pattern.FactoryMethod
+{
+    public enum NPCType
+    {
+        Farmer,
+        Beggar,
+        Shopowner
+    }
+}
